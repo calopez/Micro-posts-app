1af71b122faf5f5c198771695a80fd3511bc3e33
Carlos A. Lopez <karloslopez@icloud.com>
2014-06-01 11:15:13 -0500

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) } 
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -120,4 +121,15 @@ describe User do
     end
   end
 
+  # --------------------------------------
+  # SESSIONS
+  # --------------------------------------
+  
+  describe "remember token" do
+    before { @user.save }
+    # its is like 'it' but applies the subsequent test to the given attribute
+    # rahter than the subject of the test.
+    # it is equivalent to: it { expect(@user.remember_token).not_to be_blank }
+    its(:remember_token) { should_not be_blank }
+  end
 end
